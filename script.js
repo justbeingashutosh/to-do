1584f4eb4819680d250385101ce012775a81be61
ShadyLane <itsashutosh.dev@gmail.com>
2024-10-26 02:01:56 +0530

    Added mechanism for changing tabs

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// function updateDone(){
+//     let doneCount = doneTab.children.length;
+//     if (doneCount >= 0){
+//         let doneCounter = document.createElement('div')
+//         doneCounter.className = "doneCounter"
+//         doneCounter.textContent = `Done ${doneCount}`
+//     } 
+
+// }
+
+
 function openTodo(){
-    
+    todoTab.style.visibility = 'visible'
+    doneTab.style.visibility = 'hidden'
+    todo.style.border = 'solid white 3px'
+    done.style.border = 'none'
 }
 function openDone(){
-
+    done.style.border = 'solid white 3px'
+    todo.style.border = 'none'
+    todoTab.style.visibility = 'hidden'
+    doneTab.style.visibility = 'visible'
 }
 function createtask(){
     taskWin.style.visibility = 'visible'
@@ -12,18 +29,32 @@ function savetask(){
     task = taskInput.value.toString()
 
     let newTask = document.createElement('div')
+    let taskContent = document.createElement('div')
+    taskContent.className = 'taskcontent'
     newTask.className = "task"
-    newTask.textContent = task
+    taskContent.textContent = task
     todoTab.appendChild(newTask)
 
     let doneButton = document.createElement('button');
     doneButton.textContent = "Done";
     doneButton.className = "done-btn";
+    newTask.appendChild(taskContent)
     newTask.appendChild(doneButton)
 
     
     doneButton.addEventListener('click', ()=>{
         doneTab.prepend(newTask)
+        doneButton.remove()
+        let undo = document.createElement('button');
+        undo.textContent = 'Undo';
+        undo.className = 'undo';
+        newTask.appendChild(undo)
+        undo.addEventListener('click', ()=>{
+            todoTab.appendChild(newTask)
+            undo.remove()
+            newTask.appendChild(doneButton)
+        })
+        updateDone()
     })
     console.log(task)//for debugging
 }
@@ -33,6 +64,8 @@ function canceltask(){
 
 const doneTab = document.getElementById('done-tab')
 const todoTab =document.getElementById('todo-tab')
+const done = document.getElementById('done')
+const todo = document.getElementById('todo')
 const taskInput = document.getElementById('newtask')
 const taskWin = document.getElementById('task-win')
 document.getElementById('todo').addEventListener('click', openTodo);
